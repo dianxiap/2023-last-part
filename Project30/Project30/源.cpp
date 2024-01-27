e35a5693fbe823b87807f2d4cfc7fe9c741e025e
dianxiap <1207864396@qq.com>
2024-01-27 15:58:27 +0800

    stream

---
+++ Project30/Project30/源.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+class Account {
+public:
+    void calculate() { amount += amount * interestRate; }
+    static double rate() { return interestRate; }
+    static void rate(double newRate) { interestRate = newRate; }
+
+private:
+    std::string owner;
+    double amount;
+    static double interestRate;
+    static constexpr double todayRate = 42.42;
+    static double initRate() { return todayRate; }
+};
+
+double Account::interestRate = initRate();
+
+
+int main()
+{
+    
+    /*cout << "hh" << endl;
+    cout << "hh" << flush;
+    cout << "hh" << ends;*/
+
+}
